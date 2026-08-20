5f33d8a393724b2357adf7ec52b8d745032ad161
Albano Supermercado <albanosupermercado8@netizen.com>
2026-08-20 13:53:55 -0300

    Integra Firebase Firestore: histórico, configurações e itens do checklist agora sincronizam entre lojas/dispositivos em tempo real

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "checklist-albano-v11";
+const CACHE_NAME = "checklist-albano-v15";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./logo.png", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
